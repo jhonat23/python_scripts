b8b1a0a07e00418b1dd9ef8b76e7a418e84734b8
jhonat23 <jhonat23@gmail.com>
2022-06-29 09:32:51 -0500

    some easy challenges now in folders

---
+++ lists/tuples.py
@@ -0,0 +1,7 @@
+if __name__ == '__main__':
+   n = int(input())
+   integer_list = list(map(int, input().split()))
+   hash_tup = hash(tuple(integer_list))
+   print(hash_tup)
+#print(input()==0 or hash(tuple(map(int,input().strip().split()))))
+    
