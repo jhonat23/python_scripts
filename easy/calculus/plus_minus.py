62ea010e120ee0bb07303857ac7daa98cfd3f9bd
jhonat23 <jhonat23@gmail.com>
2022-07-25 20:12:00 -0500

    soem easy challenges

--- easy/calculus/plus_minus.py
+++ easy/calculus/plus_minus.py
@@ -1,4 +1,11 @@
-"""Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal."""
+"""Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with 6 places after the decimal.
+Example:
+Input = [1, 1, 0, -1, -1]
+Output:
+0.400000 (+)
+0.400000 (-)
+0.200000 (0)
+"""
 
 def plusMinus(arr):
     return print('{:.6f}\n'.format(len([i for i in arr if i > 0]) / len(arr)),'{:.6f}\n'.format(len([i for i in arr if i < 0]) / len(arr)),'{:.6f}\n'.format(len([i for i in arr if i == 0]) / len(arr)))
